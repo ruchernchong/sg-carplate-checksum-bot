39077e8a1703de3319f00a30eb5dec440355e776
Ru Chern Chong <hello@ruchern.dev>
2024-11-16 00:10:21 +0800

    Add checksum generator function

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { Resource } from "sst";
 import { Telegraf } from "telegraf";
+import { API_URL } from "./config";
 
 const bot = new Telegraf(Resource.TelegramBotToken.value);
 
 bot.command("start", (ctx) => ctx.reply("Welcome!"));
 
-const matchRegex = (pattern, handler) => (ctx, next) => {
+const matchRegex = (pattern: RegExp, handler) => (ctx, next) => {
   const message = ctx.message?.text;
   if (!message) return next();
 
@@ -18,21 +19,58 @@ const matchRegex = (pattern, handler) => (ctx, next) => {
 };
 
 bot.use(
-  matchRegex(/^[A-Z]{1,3}\s?\d{1,4}$/, async (ctx) => {
-    const plate = ctx.state.matches[0];
+  matchRegex(/^[A-Z]{1,3}\s?\d{1,4}$/i, async (ctx) => {
+    const plate = ctx.state.matches.input.toUpperCase();
 
-    const result: any = await fetch(
-      `https://sg-carplate-checksum-dev-honoscript.ruchernchong.workers.dev/checksum?plate=${plate}`,
-    ).then((res) => res.json());
+    const { isSpecialPlate, checksum, description, category, vehicleNo } =
+      (await fetch(`${API_URL}/checksum?plate=${plate}`).then((res) =>
+        res.json(),
+      )) as {
+        isSpecialPlate: boolean;
+        description: string;
+        category: string;
+        checksum: string;
+        vehicleNo: string;
+      };
 
-    const licensePlate = plate + result.checksum;
-    const reply = `Checksum: ${result.checksum}\n\nLicense Plate: ${licensePlate}`;
+    let text =
+      `🔍 Vehicle Search Results\n\n` +
+      `✅ Checksum: ${checksum}\n` +
+      `🚗 Vehicle Number: ${vehicleNo}`;
 
-    return ctx.reply(reply, { reply_to_message_id: ctx.message.message_id });
+    if (isSpecialPlate) {
+      text =
+        `🔰 Special Registration Numbers\n\n` +
+        `🏢 Category: ${category}\n` +
+        `📝 Description: ${description}\n` +
+        `⚠️ Note: ${checksum}\n` +
+        `🚗 Vehicle Number: ${vehicleNo}`;
+    }
+
+    console.log(text);
+
+    const checksumOffset = text.indexOf(checksum);
+    const vehicleNumberOffset = text.lastIndexOf(vehicleNo);
+
+    ctx.reply(text, {
+      reply_to_message_id: ctx.message.message_id,
+      entities: [
+        {
+          type: "bold",
+          offset: checksumOffset,
+          length: checksum.length,
+        },
+        {
+          type: "bold",
+          offset: vehicleNumberOffset,
+          length: vehicleNo.length,
+        },
+      ],
+    });
   }),
 );
 
-export const handler = async (event) => {
+export const handler = async (event: { body: string }) => {
   const body = JSON.parse(event.body);
   await bot.handleUpdate(body);
 
